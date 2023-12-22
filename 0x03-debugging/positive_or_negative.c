772ed9acfb2087db30f2dd91ec1c086a600bac4d
Chimaobi Ubani <wiztherealist@gmail.com>
2023-12-22 09:22:42 -0800

    positive or negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Entry point
+ *
+ * @i: integer variable
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+}
