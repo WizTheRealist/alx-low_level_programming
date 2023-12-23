932e0dfad3d54f914609c3b643fab27b7e42f2a6
Chimaobi Ubani <wiztherealist@gmail.com>
2023-12-23 08:12:04 -0800

    double digits combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+			putchar(i);
+			putchar(j);
+
+			if (i == '8' || j == '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
