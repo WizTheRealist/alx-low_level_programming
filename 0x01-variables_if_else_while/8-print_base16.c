aa7f862f807e25df0213857ba959cf962f8a895c
Chimaobi Ubani <wiztherealist@gmail.com>
2023-12-23 00:32:35 -0800

    putchar hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+	char n;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
