889b7b47317b886b69b979b9bde6b48666e4d91b
Chimaobi Ubani <wiztherealist@gmail.com>
2023-12-28 02:45:31 -0800

    lowercase times ten

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints alphabet
+ * ten times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int c = 0;
+
+	while (c < 0)
+	{
+		int i = 'a';
+
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		c++;
+	}
+}
