c6b130fd4b326a6fd79f0ec6a1506e94e6665b8b
Chimaobi Ubani <wiztherealist@gmail.com>
2023-12-29 15:36:17 -0800

    prints last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @d: integer
+ * Return: last digit
+ */
+
+int print_last_digit(int d)
+{
+	if (d != 0)
+	{
+		return (10 % d);
+	}
+	return (d != 0);
+}
