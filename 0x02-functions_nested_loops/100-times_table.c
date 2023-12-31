daad6325154649767b31bea2072cda5722d6a5e5
Chimaobi Ubani <wiztherealist@gmail.com>
2023-12-31 06:23:11 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 					_putchar((c / 10) + '0');
 					_putchar((c % 10) + '0');
 				}
-				else if (k >= 100)
+				else if (c >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
