cfb2ffc4209f3b9f5b75040786cb34570012b647
Chimaobi Ubani <wiztherealist@gmail.com>
2023-12-31 10:14:59 -0800

    fibonacci 1 to 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints fibonacci 1 to 98
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int count = 3;
+
+	long int a = 1, b = 2, next = a + b;
+
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+
+	while (count <= 98)
+	{
+		if (count == 98)
+		{
+			printf("%lu\n", next);
+		}
+		else
+		{
+			printf("%lu, ", next);
+		}
+		next = a + b;
+		a = b;
+		b = next;
+		count++;
+	}
+	return (0);
+}
