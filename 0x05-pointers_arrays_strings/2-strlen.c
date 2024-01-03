9d57364e09faccc5971fa384ba1481aac295fddc
Chimaobi Ubani <wiztherealist@gmail.com>
2024-01-02 23:43:52 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns string length
+ * @s: char
+ * Return: strlen
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
