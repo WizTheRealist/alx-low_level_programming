43fddf175458f348894a14e80cf2d2b0479e2c0c
Chimaobi Ubani <wiztherealist@gmail.com>
2024-01-03 06:38:11 -0800

    string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string then reverses it
+ * @s: char
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+
+	int i;
+
+	while (*(s + length) != '\0')
+	{
+		length++;
+		_putchar(*(s + length);
+	}
+	_putchar('\n');
+
+	for (i = length - i; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	putchar('\n');
