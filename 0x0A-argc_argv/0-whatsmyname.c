980e46434cb14fa62c28f252655d060d72491217
Chimaobi Ubani <wiztherealist@gmail.com>
2024-01-14 00:24:49 -0800

    arg[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entrypoint
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	int i = 0;
+
+	(void) argc;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
