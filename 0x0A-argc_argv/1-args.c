56ee9c93d47a9995be58ae7efbaabb0fe3cde5a5
Chimaobi Ubani <wiztherealist@gmail.com>
2024-01-14 01:26:00 -0800

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entrypoint
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
