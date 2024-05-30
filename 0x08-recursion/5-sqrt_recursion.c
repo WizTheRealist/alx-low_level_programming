84e2ca5c692ba6d5a50c60bd37c3166f3dd51ab1
Chimaobi <wiztherealist@gmail.com>
2024-05-30 05:40:41 -0700

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int _sqrt_recursion_wrapper(int n, int max);
+
+/**
+ * _sqrt_recursion - main entry
+ * @n: input integer
+ * Return: recurstion value
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	if (n < 0)
+		return (-1);
+	if (n < 10000)
+		return (_sqrt_recursion_wrapper(n, 100));
+	if (n < 100000000 && n > 10000)
+		return (_sqrt_recursion_wrapper(n, 10000));
+	return (_sqrt_recursion_wrapper(n, n / 2));
+}
+
+/**
+ * _sqrt_recursion_wrapper - calls
+ *
+ * @n: input integer
+ * @max: maximium value
+ * Return: integer
+ */
+
+int _sqrt_recursion_wrapper(int n, int max)
+{
+	if (max * max == n)
+		return (max);
+	if (max == 0)
+		return (-1);
+
+	return (_sqrt_recursion_wrapper(n, max - 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int _is_prime_number_wrapper(int n, int max);
+
+/**
+ * is_prime_number - entrypoint
+ *
+ * @n: input integer
+ * Return: int
+ */
+
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (_is_prime_number_wrapper(n, n / 2));
+}
+
+
+/**
+ * _is_prime_number_wrapper - main function
+ *
+ * @n: input n
+ * @max: maximium intger
+ * Return: int
+ */
+
+
+int _is_prime_number_wrapper(int n, int max)
+{
+	if (max == 1)
+		return (1);
+	if (n % max == 0 && max > 0)
+		return (0);
+	return (_is_prime_number_wrapper(n, max - 1));
+}
